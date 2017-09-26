a8bdb880b9abf5ce9dd8991f5a920c7262130c24
Glory <glory.0808>
2017-09-26 15:18:38 +0800

    使用hbm.xml配置基本映射(多对一)

--- src/main/java/com/ed/action/MappingTest.java
+++ src/main/java/com/ed/action/MappingTest.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 
 public class MappingTest {
     public static void main(String[] args) {
@@ -15,27 +17,29 @@ public class MappingTest {
         Session session = sessionFactory.getCurrentSession();
         session.getTransaction().begin();
 
-//        Person person = new Person();
-//        person.setName("李四");
-//        person.setTime(new Date());
-//        session.save(person);
-        Person person = session.get(Person.class, "297e45075ebcf01e015ebcf0255e0000");
-//        Books books = new Books();
-//        books.setName("Java从入门到放弃");
-//        books.setTime(new Date());
-//        books.setMaster(person);
-//        session.save(books);
-//        Books books1 = new Books();
-//        books1.setName("Java从入门到放弃再到入门");
-//        books1.setTime(new Date());
-//        books1.setMaster(person);
-//        session.save(books1);
-        System.out.println("-----------------------------");
-        System.out.println(person.getName());
-        System.out.println("-----------------------------");
-        for (Books books : person.getBooks()) {
-            System.out.println(books.getName());
-        }
+        Person person = new Person();
+        person.setName("tom");
+        person.setTime(new Date());
+        session.save(person);
+//        Person person = session.get(Person.class, "297e45075ebcf01e015ebcf0255e0000");
+        Books books = new Books();
+        books.setName("想个名不容易");
+        books.setTime(new Date());
+        books.setMaster(person);
+        Books books1 = new Books();
+        books1.setName("确实不容易");
+        books1.setTime(new Date());
+        books1.setMaster(person);
+
+        session.save(books);
+        session.save(books1);
+
+//        System.out.println("-----------------------------");
+//        System.out.println(person.getName());
+//        System.out.println("-----------------------------");
+//        for (Books books : person.getBooks()) {
+//            System.out.println(books.getName());
+//        }
 
         session.getTransaction().commit();
         sessionFactory.close();
