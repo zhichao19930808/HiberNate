b662f601146da7f8d9cc3ade0b6e5ce000b09a73
Glory <glory.0808>
2017-09-26 11:22:47 +0800

    使用hbm.xml配置基本映射

--- src/main/java/com/ed/action/MappingTest.java
+++ src/main/java/com/ed/action/MappingTest.java
@@ -1,6 +1,7 @@
 package com.ed.action;
 
 import com.ed.action.entity.mapping.Books;
+import com.ed.action.entity.mapping.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -16,10 +17,16 @@ public class MappingTest {
         Session session = sessionFactory.getCurrentSession();
         session.getTransaction().begin();
 
-//        Books book = new Books();
-//        book.setName("Java从入门到放弃");
-//        book.setTime(new Date());
-//        session.save(book);
+        Person person = new Person();
+        person.setName("李四");
+        person.setTime(new Date());
+        session.save(person);
+
+        Books books = new Books();
+        books.setName("Java从入门到放弃");
+        books.setTime(new Date());
+        books.setMaster(person);
+        session.save(books);
 
 
         session.getTransaction().commit();
